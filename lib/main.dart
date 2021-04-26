c9b6bd8037c1a4ced110d1ea7f90bedff9f8deea
Eugene <sokol.joe@gmail.com>
2021-04-26 23:47:14 +0300

    buttons and functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -5,15 +7,42 @@ void main() => runApp(MyApp());
 //
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer chosen!');
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color',
+      'What\'s your favorite animal'
+    ];
     return MaterialApp(
       home: SafeArea(
         child: Scaffold(
           appBar: AppBar(
             title: Text('First app!'),
           ),
-          body: Text('Default text!'),
+          body: Column(
+            children: [
+              Text('The question!'),
+              ElevatedButton(
+                onPressed: answerQuestion,
+                child: Text('Answer 1'),
+              ),
+              ElevatedButton(
+                onPressed: () => print('Answer 2 chosen'),
+                child: Text('Answer 2'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  //
+                  print('Answer 3 chosen');
+                },
+                child: Text('Answer 3'),
+              ),
+            ],
+          ),
         ),
       ),
     );
