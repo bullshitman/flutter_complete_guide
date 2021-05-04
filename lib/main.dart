f51ad748c2d24d8de05ef46b10eb553ca714a9ed
BullshitMan <sokol.joe@gmail.com>
2021-05-04 22:23:46 +0300

    Result page got score now

--- lib/main.dart
+++ lib/main.dart
@@ -69,7 +69,7 @@ class _MyAppState extends State<MyApp> {
                   questions: _questions,
                   questionIndex: _questionIndex,
                 )
-              : Result(),
+              : Result(_totalScore),
         ),
       ),
     );
--- lib/result.dart
+++ lib/result.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final int score;
+  Result(this.score);
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('You did it!'),
+      child: Text('You did it! Your score is $score'),
     );
   }
 }
